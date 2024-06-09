d4779c74cdba105fc5fe432b6afb6cff298e1e6b
SEMaUX <163045756+SEMaUX@users.noreply.github.com>
2024-06-09 20:14:01 +0200

    dodanie plikow js

---
+++ js/script-typyzmiennych.js
@@ -0,0 +1,19 @@
+console.log("Uczę się JavaScript")
+document.getElementById('paragraf').innerHTML = 'Hello World!'
+
+let a = 4 // Number (liczba)
+let b = "JavaScript" // String (wartość pomiędzy "" lub '')
+let c = 'JavaScript' // String (wartość pomiędzy "" lub '')
+
+let d = true // boolean (logiczny: true albo false)
+
+let e = null // object, null (dosłownie nic)
+
+let f // undefined
+
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+console.log(typeof(e))
+console.log(typeof(f))
